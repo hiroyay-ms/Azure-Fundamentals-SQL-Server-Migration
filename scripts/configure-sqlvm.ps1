e540ac33d7c8b5a176aefb2da0ccb733dd3c807e
Hiroya Yamamoto <hiroyay@microsoft.com>
2023-08-30 15:27:38 +0900

    Update scripts

--- scripts/configure-sqlvm.ps1
+++ scripts/configure-sqlvm.ps1
@@ -22,12 +22,5 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 Start-Sleep -s 10
 
 # Restore Database
-function Restore-Database {
-    $serverName = 'localhost'
-    $databaseName = 'AdventureWorksLT2014'
-
-    $cmd = "RESTORE DATABASE [" + $databaseName + "] FROM  DISK = N'C:\_work\AdventureWorksLT2014.bak' WITH  FILE = 2,  MOVE N'AdventureWorksLT2008_Data' TO N'F:\data\AdventureWorksLT2012_Data.mdf',  MOVE N'AdventureWorksLT2008_Log' TO N'F:\log\AdventureWorksLT2012_log.ldf',  NOUNLOAD,  STATS = 5"
-    Invoke-SqlCmd -ServerInstance $serverName -Database "master" -Query $cmd
-}
-
-Restore-Database
+$cmd = "RESTORE DATABASE [AdventureWorksLT2014] FROM  DISK = N'C:\_work\AdventureWorksLT2014.bak' WITH  FILE = 2,  MOVE N'AdventureWorksLT2008_Data' TO N'F:\data\AdventureWorksLT2012_Data.mdf',  MOVE N'AdventureWorksLT2008_Log' TO N'F:\log\AdventureWorksLT2012_log.ldf',  NOUNLOAD,  STATS = 5"
+Invoke-SqlCmd -ServerInstance "localhost" -Database "master" -Query $cmd
